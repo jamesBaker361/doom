a4530f94c29de037b389c9ade86715f31f6dbd15
James Baker <jlbaker361@gmail.com>
2025-07-05 08:48:39 -0400

    sdl lua

---
+++ 2.32.4-jlb638.lua
@@ -0,0 +1,18 @@
+help(
+[[
+Simple DirectMedia Layer is a cross-platform development
+ library designed to provide low level access to audio,
+  keyboard, mouse, joystick, and graphics hardware via OpenGL and Direct3D.
+]])
+
+whatis("a cross-platform library designed to make it easy to write multi-media software, such as games and emulators")
+
+
+
+local base = pathJoin("/projects/community/sdl/2.32.4/jlb638/")
+prepend_path("PATH", pathJoin(base, "bin"))
+prepend_path("LIBRARY_PATH", pathJoin(base, "lib"))
+prepend_path("CPATH", pathJoin(base, "include"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base, "lib"))
+
+load("gcc/5.4")
