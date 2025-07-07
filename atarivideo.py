ab1a0948f84abf39bc7006f6db5bdbe5c829a6be
James Baker <jlbaker361@gmail.com>
2025-07-07 15:41:11 -0400

    log file name

--- atarivideo.py
+++ atarivideo.py
@@ -10,21 +10,31 @@ from stable_baselines3.common.callbacks import BaseCallback
 from PIL import Image
 import os
 import csv
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--game",type=str,default="ALE/Alien-v5")
+parser.add_argument("--mode",type=int,default=0)
+parser.add_argument("--difficulty",type=int,default=0)
+parser.add_argument("--timesteps",type=int,default=100)
+
+CSV_NAME="actions.csv"
+FRAME_DIR="frames"
 
 class FrameActionPerEpisodeLogger(BaseCallback):
     def __init__(self, save_freq: int, save_dir: str, verbose: int = 0):
         super().__init__(verbose)
         self.save_freq = save_freq
         self.save_dir = save_dir
-        self.frame_dir = os.path.join(save_dir, "frames")
-        self.csv_path = os.path.join(save_dir, "actions.csv")
+        self.frame_dir = os.path.join(save_dir, FRAME_DIR)
+        self.csv_path = os.path.join(save_dir, CSV_NAME)
         os.makedirs(self.frame_dir, exist_ok=True)
         self.episode_idx = 0
         self.frame_idx = 0  # frame index within episode
 
         with open(self.csv_path, mode="w", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow(["episode", "frame_in_episode", "action"])
+            writer.writerow(["episode", "frame_in_episode", "action","file"])
 
     def _on_step(self) -> bool:
         # Environment is vectorized; assume single environment
@@ -46,16 +56,19 @@ class FrameActionPerEpisodeLogger(BaseCallback):
             action = self.locals["actions"][0]
             with open(self.csv_path, mode="a", newline="") as f:
                 writer = csv.writer(f)
-                writer.writerow([self.episode_idx, self.frame_idx, int(action)])
+                writer.writerow([self.episode_idx, self.frame_idx, int(action),filename])
 
             self.frame_idx += 1
 
         return True
 
-
+args=parser.parse_args()
 gymnasium.register_envs(ale_py)
 
-env = gymnasium.make("ALE/Alien-v5", render_mode="rgb_array")
+env = gymnasium.make(args.name, render_mode="rgb_array",
+                     full_action_space=True,
+                     difficulty=args.difficulty,
+                     mode=args.mode)
 '''env = gymnasium.wrappers.RecordVideo(
     env,
     episode_trigger=lambda num: num % 1 == 0,
@@ -63,12 +76,14 @@ env = gymnasium.make("ALE/Alien-v5", render_mode="rgb_array")
     name_prefix="video-",
 )'''
 
+FOLDER_NAME=args.name.split("/")[-1]
+
 callback = FrameActionPerEpisodeLogger(
     save_freq=1,           # Save every frame; increase if needed
-    save_dir="log_with_episodes"
+    save_dir=FOLDER_NAME
 )
 
 model = PPO("CnnPolicy", env, verbose=1)
 
-model.learn(1000,callback=callback)
+model.learn(args.timesteps,callback=callback)
 
