aca10fd868271b1c2339ecb2d42959eeb61d68a9
James Baker <jlbaker361@gmail.com>
2025-07-09 16:43:42 -0400

    sonic videos

---
+++ scripts/gen_sonic_videos.py
@@ -0,0 +1,9 @@
+for scenario in ["AquaticRuinZone.Act1",
+                  "CasinoNightZone.Act1",
+                    "ChemicalPlantZone.Act1",
+                    "EmeraldHillZone.Act1",
+                    "HillTopZone.Act1",
+                    "MetropolisZone.Act1"]:
+    command=f"sbatch -J sonic --err=slurm_chip/sonic/{scenario}.err --out=slurm_chip/sonic/{scenario}.out retrovideo.py --scenario {scenario} --timesteps 1000000 "
+    print(command)
+    
