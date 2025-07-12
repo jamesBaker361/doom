c176dac0ff8a256a2e58061d77839d28ac165f21
James Baker <jlbaker361@gmail.com>
2025-07-12 14:23:22 -0400

    sonic vae

--- scripts/gen_sonic_vae.py
+++ scripts/gen_sonic_vae.py
@@ -10,4 +10,5 @@ image_folder_path_list=[base_path+expanded_path for expanded_path in [
 
 command=f"sbatch -J vae --err=slurm_chip/vae/sonic.err --out=slurm_chip/vae/sonic.out runpygpu_chip.sh vae.py --image_folder_paths "
 command+=" ".join(image_folder_path_list)
-command+=f" --batch_size 2 --gradient_accumulation_steps 16  --limit -1 --epochs 20 --image_interval 5 --name jlbaker361/vaetest "
+command+=f" --batch_size 2 --gradient_accumulation_steps 16  --limit -1 --epochs 20 --image_interval 5 --name jlbaker361/sonic-vae1.0 "
+print(command)
