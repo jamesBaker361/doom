a15903de8934b688ce5121b540cdef22c1a05675
James Baker <jlbaker361@gmail.com>
2025-07-11 22:16:50 -0400

    more sonic videos

--- scripts/gen_sonic_videos.py
+++ scripts/gen_sonic_videos.py
@@ -1,9 +1,11 @@
-for scenario in ["AquaticRuinZone.Act1",
-                  "CasinoNightZone.Act1",
-                    "ChemicalPlantZone.Act1",
-                    "EmeraldHillZone.Act1",
-                    "HillTopZone.Act1",
-                    "MetropolisZone.Act1"]:
-    command=f"sbatch -J sonic --err=slurm_chip/sonic/{scenario}.err --out=slurm_chip/sonic/{scenario}.out runpygpu_chip.sh retrovideo.py --scenario {scenario} --timesteps 1000000 "
-    print(command)
+for num in [1,2]:
+  for _scenario in ["AquaticRuinZone.Act",
+                    "CasinoNightZone.Act",
+                      "ChemicalPlantZone.Act",
+                      "EmeraldHillZone.Act",
+                      "HillTopZone.Act",
+                      "MetropolisZone.Act"]:
+      scenario=f"{_scenario}{num}"
+      command=f"sbatch -J sonic --err=slurm_chip/sonic/{scenario}.err --out=slurm_chip/sonic/{scenario}.out runpygpu_chip.sh retrovideo.py --scenario {scenario} --timesteps 1000000 "
+      print(command)
     
