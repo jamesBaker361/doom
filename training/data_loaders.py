eef83077f386914dc3f897f0ee18c758658a4e60
James Baker <jlbaker361@gmail.com>
2025-07-14 15:30:43 -0400

    data loader for videos?

--- training/data_loaders.py
+++ training/data_loaders.py
@@ -2,6 +2,9 @@ import os
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import pandas as pd
+from PIL import Image
+import torch
 
 class FlatImageFolder(Dataset):
     def __init__(self, folder, transform=None):
@@ -17,3 +20,41 @@ class FlatImageFolder(Dataset):
         if self.transform:
             img = self.transform(img)
         return img
+    
+
+class MovieImageFolder(Dataset):
+
+    def __init__(self,folder,vae,image_processor,lookback:int):
+        super().__init__()
+        csv_file=os.path.join(folder,"actions.csv")
+        self.df=pd.read_csv(csv_file)
+        self.posterior_list=[] #images are stored from 0=t_0, 1=t_1
+        self.lookback=lookback
+        
+        for f,file in enumerate( self.df["file"]):
+            
+            pil_image=Image.open(os.path.join(folder,file))
+            pt_image=image_processor.preprocess(pil_image)
+            posterior=vae.encode(pt_image).latent_dist
+            self.posterior_list.append(posterior)
+            if f ==0:
+                self.zero_posterior=torch.zeros(posterior.size())
+        
+    def __len__(self):
+        return len(self.posterior_list)
+    
+    def __getitem__(self, index):
+        episode=self.df.iloc[index]["episode"]
+        start=index-self.lookback
+        output_dict={column:[] for column in self.df.columns}
+        output_dict["posterior"]=[]
+        for i in range(start,index):
+            if i<0 or self.df.iloc[i]["episode"]!=episode:
+                for column in self.df.columns:
+                    output_dict[column].append(-1)
+                output_dict["posterior"].append(self.zero_posterior)
+            else:
+                for column in self.df.columns:
+                    output_dict[column].append(self.df.iloc[i][column])
+                output_dict["posterior"].append(self.posterior_list[i])
+        return output_dict
