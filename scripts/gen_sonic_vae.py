cca1379def65819b5c3bcb84e5fec35902c5dc67
James Baker <jlbaker361@gmail.com>
2025-07-13 22:38:32 -0400

    formatiing

--- scripts/gen_sonic_vae.py
+++ scripts/gen_sonic_vae.py
@@ -18,14 +18,14 @@ for gpus in [1,2]:
    ]:
         
         zone_name=zone.split("/")[0]
-        command=f"sbatch -J vae --err=slurm_chip/vae{gpus}_{zone_name}/sonic.err --out=slurm_chip/vae{gpus}_{zone_name}/sonic.out"
+        command=f"sbatch -J vae --err=slurm_chip/vae{gpus}_{zone_name}/sonic.err --out=slurm_chip/vae{gpus}_{zone_name}/sonic.out "
         if gpus!=1:
-            command+=f"--gres=gpu:{gpus} "
+            command+=f" --gres=gpu:{gpus} "
         command+=" runaccgpu_chip.sh "
         if gpus!=1:
             command+=f" --multigpu --num_processes {gpus} "
         command+=f" training/vae.py --image_folder_paths "
         command+=f" {base_path}{zone} "
-        command+=f" --batch_size 2 --gradient_accumulation_steps 16  --limit -1 --epochs 20 --image_interval 5 --name jlbaker361/sonic-vae{gpus}-{zone} "
+        command+=f" --batch_size 2 --gradient_accumulation_steps 16  --limit -1 --epochs 20 --image_interval 5 --name jlbaker361/sonic-vae{gpus}-{zone_name} "
         command+=" --project_name vae "
         print(command)
