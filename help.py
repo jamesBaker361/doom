db660fdc19ffc1c590a403c7def42b36ab99cf67
James Baker <jlbaker361@gmail.com>
2025-07-15 14:20:39 -0400

    device

--- help.py
+++ help.py
@@ -1,9 +1,12 @@
 from training.data_loaders import MovieImageFolder
 from diffusers import AutoencoderKL
 from diffusers.image_processor import VaeImageProcessor
+import torch
 
 folder="sonic_videos_10/SonicTheHedgehog2-Genesis/EmeraldHillZone.Act1/gelly-religiousness-brazos/"
 vae=AutoencoderKL.from_pretrained("SimianLuo/LCM_Dreamshaper_v7",subfolder="vae")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+vae=vae.to(device)
 
 image_processor=VaeImageProcessor(vae_scale_factor=8)
 data=MovieImageFolder(folder,vae,image_processor,4)
