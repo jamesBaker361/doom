d2134f628a0e53c2d4465778ac8a5752d834bece
James Baker <jlbaker361@gmail.com>
2025-07-16 17:45:34 -0400

    upload

--- make_dataset.py
+++ make_dataset.py
@@ -1,6 +1,6 @@
 import torch
 import huggingface_hub
-import datasets
+from datasets import Dataset
 import argparse
 from accelerate import Accelerator
 from diffusers import LCMScheduler,DiffusionPipeline,DEISMultistepScheduler,DDIMScheduler,SCMScheduler,AutoencoderDC,AutoencoderKL,UNet2DConditionModel
@@ -9,6 +9,7 @@ from diffusers.schedulers.scheduling_lcm import LCMScheduler
 from diffusers.image_processor import VaeImageProcessor
 import os
 import pandas as pd
+from PIL import Image
 
 parser=argparse.ArgumentParser()
 parser.add_argument("--vae_checkpoint",type=str,default="SimianLuo/LCM_Dreamshaper_v7")
@@ -27,5 +28,20 @@ image_processor=VaeImageProcessor(vae_scale_factor=8)
 csv_file = os.path.join(args.folder, "actions.csv")
 df=pd.read_csv(csv_file)
 
+columns=df.columns
+print(columns)
+
+output_dict=df.to_dict("list")
+posterior_list = []
+
+for file in output_dict["file"]:
+    pil_image = Image.open(os.path.join(args.folder, file))
+    pt_image = image_processor.preprocess(pil_image)
+    posterior = vae.encode(pt_image.to(vae.device)).latent_dist.parameters.cpu().detach()
+    
+    posterior_list.append(posterior)
+output_dict["posterior_list"]=posterior_list
+
+Dataset.from_dict(output_dict).push_to_hub(args.upload_path)
 
 accelerator.print("all done :) ")
