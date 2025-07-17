f32fa40d659dc6c01506574f031f17709a47b35d
James Baker <jlbaker361@gmail.com>
2025-07-17 11:44:32 -0400

    sonic sequences

---
+++ scripts/gen_sonic_sequences.py
@@ -0,0 +1,13 @@
+for timesteps in [1_000_000]:
+  for num in [1]:
+    for _scenario in ["AquaticRuinZone.Act",
+                      "CasinoNightZone.Act",
+                        "ChemicalPlantZone.Act",
+                        "EmeraldHillZone.Act",
+                        "HillTopZone.Act",
+                        "MetropolisZone.Act"]:
+        scenario=f"{_scenario}{num}"
+        command=f"sbatch -J sonic --err=slurm_chip/sonic_sequences/{scenario}{timesteps}.err --out=slurm_chip/sonic_sequences/{scenario}{timesteps}.out runpygpu_chip.sh retrovideo.py --scenario {scenario} --timesteps {timesteps} "
+        command+=f" --save_dir sonic_sequences_{timesteps} --image_saving"
+        print(command)
+    
