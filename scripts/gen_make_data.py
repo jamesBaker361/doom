002a4921378ce70b0c85aab6bb03f300bc90531e
James Baker <jlbaker361@gmail.com>
2025-07-17 11:35:57 -0400

    make pretrained data

--- scripts/gen_make_data.py
+++ scripts/gen_make_data.py
@@ -1,8 +1,8 @@
 base_path="sonic_videos_100000/SonicTheHedgehog2-Genesis/"
-for [name,path] in [
-    ["sonic_emerald_100000","EmeraldHillZone.Act1/aminoaciduria-popery-clozapine"],
-    ["sonic_hilltop_100000","HillTopZone.Act1/quamassia-cervus-plateful"],
-    ["sonic_metro_100000","MetropolisZone.Act1/sonograph-tinner-laudability"]
+for [name,path,pretrained] in [
+    ["sonic_emerald_100000_trained","EmeraldHillZone.Act1/aminoaciduria-popery-clozapine","jlbaker361/sonic-vae2-EmeraldHillZone.Act1"],
+    ["sonic_hilltop_100000_trained","HillTopZone.Act1/quamassia-cervus-plateful","jlbaker361/sonic-vae2-HillTopZone.Act1"],
+    ["sonic_metro_100000_trained","MetropolisZone.Act1/sonograph-tinner-laudability","jlbaker361/sonic-vae2-MetropolisZone.Act1"]
 ]:
     command=f"sbatch -J data --err=slurm_chip/data/{name}.err --out=slurm_chip/data/{name}.out runpygpu_chip.sh make_dataset.py "
     command+=f" --upload_path jlbaker361/{name} --folder {base_path}{path} "
