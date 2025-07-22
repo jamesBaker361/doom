9ef304385e6028abab8685ecf0044cc0af6e0000
James Baker <jlbaker361@gmail.com>
2025-07-22 11:53:09 -0400

    ????

---
+++ help.py
@@ -0,0 +1,45 @@
+import os
+import argparse
+from experiment_helpers.gpu_details import print_details
+from datasets import load_dataset
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import json
+
+import torch
+import accelerate
+from accelerate import Accelerator
+from huggingface_hub.errors import HfHubHTTPError
+from accelerate import PartialState
+import time
+import torch.nn.functional as F
+from PIL import Image
+import random
+import wandb
+import numpy as np
+import random
+from gpu_helpers import *
+from diffusers import LCMScheduler,DiffusionPipeline,DEISMultistepScheduler,DDIMScheduler,SCMScheduler,AutoencoderDC,AutoencoderKL,UNet2DConditionModel
+from diffusers.models.attention_processor import IPAdapterAttnProcessor2_0
+from diffusers.schedulers.scheduling_lcm import LCMScheduler
+from torchvision.transforms.v2 import functional as F_v2
+from torchmetrics.image.fid import FrechetInceptionDistance
+
+from transformers import AutoProcessor, CLIPModel
+try:
+    from torch.distributed.fsdp import register_fsdp_forward_method
+except ImportError:
+    print("cant import register_fsdp_forward_method")
+from diffusers.models.autoencoders.vae import DiagonalGaussianDistribution
+from huggingface_hub import create_repo,HfApi
+from data_loaders import MovieImageFolder,MovieImageFolderFromHF
+from torch.utils.data import DataLoader
+from peft import LoraConfig
+from diffusers.image_processor import VaeImageProcessor
+
+data=load_dataset("jlbaker361/sonic_emerald_100000",split="train")
+
+for row in data:
+    posterior=data["posterior_list"]
+    posterior=np.array(posterior)
+    print(posterior.shape)
