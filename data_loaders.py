bf99ab798af144590d4bf79db1fd7eb2a2861c66
James Baker <jlbaker361@gmail.com>
2025-07-23 11:12:32 -0400

    no squeez

--- data_loaders.py
+++ data_loaders.py
@@ -96,15 +96,9 @@ class MovieImageFolder(Dataset):
             if i==-1:
                 tiny_posterior_list.append(self.zero_posterior)
             else:
-                try:
-                    tiny_posterior_list.append(DiagonalGaussianDistribution(self.posterior_list[i]).sample().squeeze(0))
-                    print("DiagonalGaussianDistribution(self.posterior_list[i]).sample().squeeze(0)",DiagonalGaussianDistribution(self.posterior_list[i]).sample().squeeze(0))
-                    
-                except TypeError:
-                    print("type error!")
-                    tiny_posterior_list.append(DiagonalGaussianDistribution(torch.tensor(self.posterior_list[i]).squeeze(0)).sample().squeeze(0))
-                    print("torch.tensor(self.posterior_list[i]).squeeze(0))", torch.tensor(self.posterior_list[i]).squeeze(0).size())
-                    print("DiagonalGaussianDistribution(torch.tensor(self.posterior_list[i]).squeeze(0)).sample()", DiagonalGaussianDistribution(torch.tensor(self.posterior_list[i]).squeeze(0)).sample().size())
+                tiny_posterior_list.append(DiagonalGaussianDistribution(torch.tensor(self.posterior_list[i])).sample().squeeze(0))
+                print("torch.tensor(self.posterior_list[i]))", torch.tensor(self.posterior_list[i]).size())
+                print("DiagonalGaussianDistribution(torch.tensor(self.posterior_list[i])).sample()", DiagonalGaussianDistribution(torch.tensor(self.posterior_list[i])).sample().size())
             print(" tiny_posterior_list[-1] size? ",tiny_posterior_list[-1].size())
         output_dict["posterior"]=torch.cat(tiny_posterior_list)
         return output_dict
