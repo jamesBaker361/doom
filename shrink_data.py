ef5f34ad52a093faa4996ead9bae2b630b4f96eb
James Baker <jlbaker361@gmail.com>
2025-07-23 15:04:57 -0400

    anutha dataset

--- shrink_data.py
+++ shrink_data.py
@@ -1,12 +1,12 @@
 from datasets import load_dataset
 
-data=load_dataset("jlbaker361/sonic_emerald_100000",split="train")
+'''data=load_dataset("jlbaker361/sonic_emerald_100000",split="train")
 
 n=100
 
 small_dataset = data.shuffle(seed=42).select(range(n))
 
-small_dataset.push_to_hub("jlbaker361/sonic_100")
+small_dataset.push_to_hub("jlbaker361/sonic_100")'''
 
 #jlbaker361/sonic_hilltop_sequence
 
