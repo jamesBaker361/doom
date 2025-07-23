bfb0e9408a8306d1b2469b11e6add8ecea1815c3
James Baker <jlbaker361@gmail.com>
2025-07-23 15:09:37 -0400

    default seque

--- scripts/gen_sonic_sequences.py
+++ scripts/gen_sonic_sequences.py
@@ -8,6 +8,6 @@ for timesteps in [1_000_000]:
                         "MetropolisZone.Act"]:
         scenario=f"{_scenario}{num}"
         command=f"sbatch -J sonic --err=slurm_chip/sonic_sequences/{scenario}{timesteps}.err --out=slurm_chip/sonic_sequences/{scenario}{timesteps}.out runpygpu_chip.sh retrovideo.py --scenario {scenario} --timesteps {timesteps} "
-        command+=f" --save_dir sonic_sequences_{timesteps} --image_saving"
+        command+=f" --save_dir sonic_sequences_{timesteps} --image_saving  --sequence_dataset jlbaker361/sonic_sequence_100   "
         print(command)
     
