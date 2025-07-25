2d3784236e5d7d844c2bc78c3a787989ead2c763
James Baker <jlbaker361@gmail.com>
2025-07-25 15:31:53 -0400

    concat rnn

--- scripts/gen_sequences.py
+++ scripts/gen_sequences.py
@@ -1,4 +1,4 @@
-for model_type in ["cnn","rnn","transformer"]:
+for model_type in ["cnn","rnn","transformer","lstm","gru","concatrnn"]:
     command=f"sbatch -J seq --err=slurm/seq/{model_type}.err --out=slurm/seq/{model_type}.out runpymain.sh "
     command+=f" sequence_training.py --model_type {model_type} --metadata_keys x y  --sequence_dataset  jlbaker361/sonic_sequence_100 "
     print(command)
