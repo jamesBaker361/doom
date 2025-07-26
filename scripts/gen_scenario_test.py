51471d8d9cfe09cae48625bc87af8e8d02dbfb2f
James Baker <jlbaker361@gmail.com>
2025-07-26 09:47:03 -0400

    scenario testing

---
+++ scripts/gen_scenario_test.py
@@ -0,0 +1,8 @@
+for timesteps in [10]:
+    for ___scenario in ["AquaticRuinZone.Act",
+                        "AquaticRuinZone.Act1",
+                        "AquaticRuinZone",]:
+        scenario=f"{___scenario}"
+        command=f"sbatch -J sonic --err=slurm/sonic/{scenario}{timesteps}.err --out=slurm/sonic/{scenario}{timesteps}.out runpymain.sh retrovideo.py --scenario {scenario} --timesteps {timesteps} "
+        command+=f" --save_dir /scratch/jlb638/retro/sonic_videos_{timesteps} "
+        print(command)
