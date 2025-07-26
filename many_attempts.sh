627272512bc3d34300551ad3440e9c880ec64d51
James Baker <jlbaker361@gmail.com>
2025-07-26 13:24:13 -0400

    brute force?

---
+++ many_attempts.sh
@@ -0,0 +1,8 @@
+sbatch runpygpu_chip.sh rom_import.py
+sbatch runpygpu_chip.sh rom_import.py
+sbatch runpygpu_chip.sh rom_import.py
+sbatch runpygpu_chip.sh rom_import.py
+sbatch runpygpu_chip.sh rom_import.py
+sbatch runpygpu_chip.sh rom_import.py
+sbatch runpygpu_chip.sh rom_import.py
+sbatch runpygpu_chip.sh rom_import.py
