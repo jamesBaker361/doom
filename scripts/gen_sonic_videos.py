028031940e1e6cea15aa19863d1ff4a4461d3479
James Baker <jlbaker361@gmail.com>
2025-07-26 10:05:13 -0400

    only one scenario works :(

--- scripts/gen_sonic_videos.py
+++ scripts/gen_sonic_videos.py
@@ -1,11 +1,7 @@
-for timesteps in [1_000_000]:
+for timesteps in [2_500_000]:
   for num in [1]:
-    for ___scenario in ["AquaticRuinZone.Act",
-                      "CasinoNightZone.Act",
-                        "ChemicalPlantZone.Act",
-                        "EmeraldHillZone.Act",
-                        "HillTopZone.Act",
-                        "MetropolisZone.Act"]:
+    for ___scenario in [
+                        "EmeraldHillZone.Act",]:
         scenario=f"{___scenario}{num}"
         command=f"sbatch -J sonic --err=slurm/sonic/{scenario}{timesteps}.err --out=slurm/sonic/{scenario}{timesteps}.out runpymain.sh retrovideo.py --scenario {scenario} --timesteps {timesteps} "
         command+=f" --save_dir /scratch/jlb638/retro/sonic_videos_{timesteps} "
