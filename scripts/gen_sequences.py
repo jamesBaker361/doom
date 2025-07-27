bbb1dceb59513dcb97e50ab9a41b2af27733c46a
James Baker <jlbaker361@gmail.com>
2025-07-27 14:28:46 -0400

    1 mil training for rnn

--- scripts/gen_sequences.py
+++ scripts/gen_sequences.py
@@ -1,6 +1,7 @@
-for model_type in ["concattransformer","concatlstm","concatgru","concatrnn"]:
+for model_type in ["concattransformer","concatlstm","concatgru","concatrnn","rnn","lstm","gru","transformer"]:
     command=f"sbatch -J seq --err=slurm/seq/{model_type}.err --out=slurm/seq/{model_type}.out runpymain.sh "
-    command+=f" sequence_training.py --model_type {model_type} --metadata_keys x y  --sequence_dataset  jlbaker361/sonic_sequence_100 "
+    command+=f" sequence_training.py --model_type {model_type} --metadata_keys x y  --sequence_dataset  jlbaker361/sonic_hilltop_sequence  "
+    command+=f" --gradient_accumulation_steps 32 --batch_size 2 "
     if model_type.find("concat")!=-1:
         command+=" --use_prior  "
     print(command)
