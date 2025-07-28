2eb619e21b2c9404698a7ef09b77e3ee8fac9a82
James Baker <jlbaker361@gmail.com>
2025-07-28 16:05:11 -0400

    constraints

--- many_attempts.sh
+++ many_attempts.sh
@@ -1,8 +1,6 @@
-sbatch runpygpu_chip.sh rom_import.py
-sbatch runpygpu_chip.sh rom_import.py
-sbatch runpygpu_chip.sh rom_import.py
-sbatch runpygpu_chip.sh rom_import.py
-sbatch runpygpu_chip.sh rom_import.py
-sbatch runpygpu_chip.sh rom_import.py
-sbatch runpygpu_chip.sh rom_import.py
-sbatch runpygpu_chip.sh rom_import.py
+
+sbatch --constraint='RTX_2080TI' runpygpu_chip.sh rom_import.py
+sbatch --constraint='RTX_6000' runpygpu_chip.sh rom_import.py
+sbatch --constraint='RTX_8000' runpygpu_chip.sh rom_import.py
+sbatch --constraint='H100' runpygpu_chip.sh rom_import.py
+sbatch --constraint='L40S' runpygpu_chip.sh rom_import.py
