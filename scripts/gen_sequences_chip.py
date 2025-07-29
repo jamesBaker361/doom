0ad66c552333e4bbe24ef456557b11161ec108c1
James Baker <jlbaker361@gmail.com>
2025-07-29 16:22:30 -0400

    1mil

--- scripts/gen_sequences_chip.py
+++ scripts/gen_sequences_chip.py
@@ -1,6 +1,6 @@
 for model_type in ["concattransformer","concatlstm","concatgru","concatrnn","rnn","lstm","gru","transformer"]:
     command=f"sbatch -J seq --err=slurm_chip/seq/{model_type}.err --out=slurm_chip/seq/{model_type}.out runpygpu_chip.sh "
-    command+=f" sequence_training.py --model_type {model_type} --metadata_keys x y --sequence_dataset  jlbaker361/sonic_sequence_100 "
+    command+=f" sequence_training.py --model_type {model_type} --metadata_keys x y --sequence_dataset  jlbaker361/sonic_hilltop_sequence "
     if model_type.find("concat")!=-1:
         command+=" --use_prior  "
     print(command)
