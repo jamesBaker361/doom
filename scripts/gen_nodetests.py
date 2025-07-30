b37dbd672275594a0c36418cbe3f6ff87a63314c
James Baker <jlbaker361@gmail.com>
2025-07-29 22:15:04 -0400

    testing

--- scripts/gen_nodetests.py
+++ scripts/gen_nodetests.py
@@ -1,5 +1,8 @@
 def to_two_digit(match):
-    return f"{int(match.group()):02d}"
+    if match>9:
+        return str(match)
+    else:
+        return "0"+str(match)
 
 for node in [f"g20-{to_two_digit(n)}" for n in range(1,14)]+[f"g24-{to_two_digit(1,13)}"]:
     print(f"sbatch --nodelist={node} runpygpu_chip.sh bullshit.py ")
