74b11d309bfb09d58fd35addd33ea22611774df2
James Baker <jlbaker361@gmail.com>
2025-07-31 18:16:41 -0400

    nodelists

--- scripts/gen_sonic_videos_chip.py
+++ scripts/gen_sonic_videos_chip.py
@@ -7,7 +7,7 @@ for timesteps in [50000]:
                         "HillTopZone.Act1",
                         "MetropolisZone.Act1"]:
         scenario=f"{state}{num}"
-        command=f"sbatch -J sonic--nodelist=g20-02 --err=slurm_chip/sonic/{scenario}{timesteps}.err --out=slurm_chip/sonic/{scenario}{timesteps}.out runpygpu_chip.sh retrovideo.py --scenario {scenario} --timesteps {timesteps} "
+        command=f"sbatch -J sonic --nodelist=g20-02,g20-03,g20-04,g20-05,g20-09,g20-12 --err=slurm_chip/sonic/{scenario}{timesteps}.err --out=slurm_chip/sonic/{scenario}{timesteps}.out runpygpu_chip.sh retrovideo.py --scenario {scenario} --timesteps {timesteps} "
         command+=f" --save_dir sonic_videos_{timesteps} --state {state} "
         print(command)
     
