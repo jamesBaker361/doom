297c076d5cbe5b242cc6e5198f7f3f733c59d081
James Baker <jlbaker361@gmail.com>
2025-08-02 19:44:25 -0400

    no grad

--- make_dataset.py
+++ make_dataset.py
@@ -18,52 +18,54 @@ from huggingface_hub import create_repo,HfApi
 import torch
 import datetime
 
-parser=argparse.ArgumentParser()
-parser.add_argument("--vae_checkpoint",type=str,default="")
-parser.add_argument("--folder",type=str,default="sonic_videos_10/SonicTheHedgehog2-Genesis/EmeraldHillZone.Act1/gelly-religiousness-brazos/")
-parser.add_argument("--upload_path",type=str,default="jlbaker361/sonic10")
-parser.add_argument("--no_image",action="store_true")
+with torch.no_grad():
 
-args=parser.parse_args()
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--vae_checkpoint",type=str,default="")
+    parser.add_argument("--folder",type=str,default="sonic_videos_10/SonicTheHedgehog2-Genesis/EmeraldHillZone.Act1/gelly-religiousness-brazos/")
+    parser.add_argument("--upload_path",type=str,default="jlbaker361/sonic10")
+    parser.add_argument("--no_image",action="store_true")
 
-print(args)
+    args=parser.parse_args()
 
-accelerator=Accelerator()
-vae=AutoencoderKL.from_pretrained("digiplay/DreamShaper_7",subfolder="vae").to(accelerator.device)
-image_processor=VaeImageProcessor(vae_scale_factor=8)
+    print(args)
 
-if len(args.vae_checkpoint)>0:
-    api=HfApi()
+    accelerator=Accelerator()
+    vae=AutoencoderKL.from_pretrained("digiplay/DreamShaper_7",subfolder="vae").to(accelerator.device)
+    image_processor=VaeImageProcessor(vae_scale_factor=8)
 
-    pretrained_weights_path=api.hf_hub_download(args.vae_checkpoint,"diffusion_pytorch_model.safetensors",force_download=True)
+    if len(args.vae_checkpoint)>0:
+        api=HfApi()
 
-    autoencoder=AutoencoderKL.from_pretrained("digiplay/DreamShaper_7",subfolder="vae")
+        pretrained_weights_path=api.hf_hub_download(args.vae_checkpoint,"diffusion_pytorch_model.safetensors",force_download=True)
 
-    autoencoder.load_state_dict(torch.load(pretrained_weights_path,weights_only=True),strict=False)
+        autoencoder=AutoencoderKL.from_pretrained("digiplay/DreamShaper_7",subfolder="vae")
 
-csv_file = os.path.join(args.folder, "actions.csv")
-df=pd.read_csv(csv_file)
+        autoencoder.load_state_dict(torch.load(pretrained_weights_path,weights_only=True),strict=False)
 
-columns=df.columns
-print(columns)
+    csv_file = os.path.join(args.folder, "actions.csv")
+    df=pd.read_csv(csv_file)
 
-output_dict=df.to_dict("list")
-if args.no_image !=True:
-    posterior_list = []
-    image_list=[]
+    columns=df.columns
+    print(columns)
 
-    for n,file in enumerate(output_dict["file"]):
-        with Image.open(os.path.join(args.folder, file)) as pil_image:
-            #pt_image = image_processor.preprocess(pil_image)
-            #posterior = vae.encode(pt_image.to(vae.device)).latent_dist.parameters.cpu().detach()
-            
-            #posterior_list.append(posterior)
-            image_list.append(pil_image)
-    #output_dict["posterior_list"]=posterior_list
-        output_dict["image"]=image_list
+    output_dict=df.to_dict("list")
+    if args.no_image !=True:
+        posterior_list = []
+        image_list=[]
 
-        if n%100==1:
-            Dataset.from_dict(output_dict).push_to_hub(args.upload_path)
-            print(n, datetime.datetime.now())
+        for n,file in enumerate(output_dict["file"]):
+            with Image.open(os.path.join(args.folder, file)) as pil_image:
+                pt_image = image_processor.preprocess(pil_image)
+                posterior = vae.encode(pt_image.to(vae.device)).latent_dist.parameters.cpu().detach()
+                
+                posterior_list.append(posterior)
+                image_list.append(pil_image)
+        #output_dict["posterior_list"]=posterior_list
+            output_dict["image"]=image_list
 
-accelerator.print("all done :) ")
+            if n%100==1:
+                Dataset.from_dict(output_dict).push_to_hub(args.upload_path)
+                print(n, datetime.datetime.now())
+
+    accelerator.print("all done :) ")
