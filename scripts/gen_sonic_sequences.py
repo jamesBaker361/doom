ea7c1de2c14f5a34f70a2039927dc35bf334ab3a
James Baker <jlbaker361@gmail.com>
2025-10-04 19:56:34 -0400

    typo

--- scripts/gen_sonic_sequences.py
+++ scripts/gen_sonic_sequences.py
@@ -7,7 +7,7 @@ for timesteps in [1_000_000]:
                         "HillTopZone.Act",
                         "MetropolisZone.Act"]:
         scenario=f"{_scenario}{num}"
-        command=f"sbatch -J sonic --err=slurm_chip/sonic_sequences/{scenario}{timesteps}.err --out=slurm_chip/sonic_sequences/{scenario}{timesteps}.out runpygpu_chip.sh retrovideo.py --scenario {scenario} --timesteps {timesteps} "
+        command=f"sbatch -J sonic --err=slurm_chip/sonic_sequences/{scenario}{timesteps}.err --out=slurm_chip/sonic_sequences/{scenario}{timesteps}.out runpygpu_chip.sh retrovideo_memory.py --scenario {scenario} --timesteps {timesteps} "
         command+=f" --dest_dataset jlbaker361/{scenario}{timesteps}   "
         print(command)
     
