9296eb5d6079ef9ee0dea69906c451d583e1586e
James Baker <jlbaker361@gmail.com>
2025-10-14 15:03:04 -0400

    +1

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,12 +5,12 @@ import torch
 
 data=load_dataset("jlbaker361/sonic-vae",split="train")
 image_processor=DiffusionPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7").image_processor
-n_actions=len(set(data["action"]))
+n_actions=max(set(data["action"]))+1
 print(set(data["action"]))
 episode_set=set()
 data=data.select([z for z in range(2)])
 data=data.map(lambda x :{"image": image_processor.preprocess( x["image"])[0]})
-data=data.map(lambda x: {"action":F.one_hot(torch.Tensor(x["action"]).long(),n_actions+1)})
+data=data.map(lambda x: {"action":F.one_hot(torch.Tensor(x["action"]).long(),n_actions)})
 
 data.push_to_hub("jlbaker361/sonic-vae-preprocessed")
 
