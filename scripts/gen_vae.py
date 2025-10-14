98f7023d5b892ca9053896d1da28272d8cc67f9f
James Baker <jlbaker361@gmail.com>
2025-10-14 17:43:29 -0400

    gen ae

--- scripts/gen_vae.py
+++ scripts/gen_vae.py
@@ -5,3 +5,4 @@ for encoder_type in ["vae","vqvae"]:
             command=f" sbatch -J vae --err=slurm_chip/encoder/{name}.err --out=slurm_chip/encoder/{name}.out runpygpu_chip.sh vae.py "
             command+=f" --name  jlbaker361/sonic-encoder-{name} --save_dir sonic_encoder_{name}  --encoder_type  {encoder_type}  --project_name sonic-encoding --gradient_accumulation_steps {grad} "
             command+=f" --lr {lr} "
+            print(command)
