fd21fe8b110963694a602ee2cec396f8c1f85558
James Baker <jamesbaker@Jamess-MacBook-Air.local>
2025-10-25 17:16:08 -0400

    l40

--- scripts/gen_vae.py
+++ scripts/gen_vae.py
@@ -2,7 +2,7 @@ for encoder_type in ["vae","vqvae"]:
     for grad in [4,8]:
         for lr in [0.001, 0.0001]:
             name=f"{encoder_type}_{grad}_{lr}"
-            command=f" sbatch -J vae --err=slurm_chip/encoder/{name}.err --out=slurm_chip/encoder/{name}.out runpygpu_chip.sh vae.py "
+            command=f" sbatch -J vae --constraint=L40S --err=slurm_chip/encoder/{name}.err --out=slurm_chip/encoder/{name}.out runpygpu_chip_l40.sh vae.py "
             command+=f" --name  jlbaker361/sonic-encoder-{name} --save_dir sonic_encoder_{name}  --encoder_type  {encoder_type}  --project_name sonic-encoding --gradient_accumulation_steps {grad} "
             command+=f" --lr {lr} "
             print(command)
