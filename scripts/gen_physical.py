d25805e822c8366f8d38bee2aaa73abc519b50b2
James Baker <jamesbaker@Jamess-MacBook-Air-4.local>
2025-11-07 19:41:43 -0500

    grad

--- scripts/gen_physical.py
+++ scripts/gen_physical.py
@@ -1,4 +1,4 @@
 for image_encoder in ["vae","trained"]:
     command=f"sbatch -J phy --err=slurm_chip/physical/{image_encoder}.err --out=slurm_chip/physical/{image_encoder}.out "
-    command+=f"runpygpu_chip.sh physical_models.py --image_encoder {image_encoder} --epochs 4 --val_interval 2 "
+    command+=f"runpygpu_chip.sh physical_models.py --image_encoder {image_encoder} --epochs 100 --val_interval 10 --gradient_accumulation_steps 8 --limit -1 "
     print(command)
