f06c9913a488e5ee694541f56cd030916abd8422
James Baker <jamesbaker@ipo3912l560.rad.rutgers.edu>
2025-11-10 15:38:26 -0500

    training amarel

---
+++ scripts/gen_physical_amarel.py
@@ -0,0 +1,6 @@
+for image_encoder in ["vae","trained"]:
+    for lr in [0.001, 0.005]:
+        command=f"sbatch -J phy --err=slurm/physical/{image_encoder}_{lr}.err --out=slurm/physical/{image_encoder}_{lr}.out "
+        command+=f"runpygpu.sh physical_models.py --image_encoder {image_encoder} --epochs 100 --val_interval 10 --gradient_accumulation_steps 8 --limit 10000 "
+        command+=f" --name jlbaker361/{image_encoder}_{lr} "
+        print(command)
