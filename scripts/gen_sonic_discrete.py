67b7649a443a39058fcc58f028e3be66a983a64e
James Baker <jamesbaker@Jamess-MacBook-Air-7.local>
2025-11-10 19:53:16 -0500

    new sonic

---
+++ scripts/gen_sonic_discrete.py
@@ -0,0 +1,17 @@
+for timesteps in [100]:
+  for num in [
+      1
+      #,2
+      ]:
+    for _scenario in [
+        "AquaticRuinZone.Act",
+                    #  "CasinoNightZone.Act",
+                      #  "ChemicalPlantZone.Act",
+                      #  "EmeraldHillZone.Act",
+                      #  "HillTopZone.Act",
+                      #  "MetropolisZone.Act"
+                        ]:
+        scenario=f"{_scenario}{num}"
+        command=f"sbatch -J sonic --err=slurm_chip/sonic_sequences_discrete/{scenario}{timesteps}.err --out=slurm_chip/sonic_sequences_discrete/{scenario}{timesteps}.out runpygpu_chip.sh retrovideo_memory.py --scenario {scenario} --timesteps {timesteps} "
+        command+=f" --dest_dataset jlbaker361/discrete_{scenario}{timesteps} --max_episode_steps {timesteps}  "
+        print(command)
