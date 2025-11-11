54319f307cdaa446b491a96b2d5e37837638e6a3
James Baker <jamesbaker@Jamess-MacBook-Air-7.local>
2025-11-10 21:24:53 -0500

    many sonics

--- scripts/gen_sonic_discrete.py
+++ scripts/gen_sonic_discrete.py
@@ -1,15 +1,15 @@
-for timesteps in [100]:
+for timesteps in [10000]:
   for num in [
       1
-      #,2
+      ,2
       ]:
     for _scenario in [
         "AquaticRuinZone.Act",
-                    #  "CasinoNightZone.Act",
-                      #  "ChemicalPlantZone.Act",
-                      #  "EmeraldHillZone.Act",
-                      #  "HillTopZone.Act",
-                      #  "MetropolisZone.Act"
+                      "CasinoNightZone.Act",
+                        "ChemicalPlantZone.Act",
+                        "EmeraldHillZone.Act",
+                       "HillTopZone.Act",
+                        "MetropolisZone.Act"
                         ]:
         scenario=f"{_scenario}{num}"
         command=f"sbatch -J sonic --err=slurm_chip/sonic_sequences_discrete/{scenario}{timesteps}.err --out=slurm_chip/sonic_sequences_discrete/{scenario}{timesteps}.out runpygpu_chip.sh retrovideo_memory.py --scenario {scenario} --timesteps {timesteps} "
