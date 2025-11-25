94db2198fcb998416053185323bd9cfe2fb5d073
James Baker <jamesbaker@Jamess-MacBook-Air-10.local>
2025-11-24 19:19:43 -0500

    physical

--- scripts/gen_physical.py
+++ scripts/gen_physical.py
@@ -2,7 +2,7 @@ for image_encoder in ["vae","trained"]:
     for lr in [0.001, 0.005]:
         for zone in ["EmeraldHillZone","HillTopZone"]:
             name=f"{image_encoder}_{lr}_{zone}"
-            data=f"jlbaker361/discrete_{zone}.Act1100"
+            data=f"jlbaker361/discrete_{zone}.Act15000000" #jlbaker361/discrete_HillTopZone.Act15000000
             command=f"sbatch -J phy --err=slurm_chip/physical/{name}.err --out=slurm_chip/physical/{name}.out "
             command+=f"runpygpu_chip.sh physical_models.py --image_encoder {image_encoder} --epochs 100 --val_interval 10 --gradient_accumulation_steps 8 --limit 10000 "
             command+=f" --name jlbaker361/{image_encoder}_{lr}  --velocity_dataset {data} "
