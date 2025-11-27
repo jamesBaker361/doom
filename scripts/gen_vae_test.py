eb48430fcfa5d2c798dfb62215f2e7da725bdf55
James Baker <jamesbaker@Jamess-MacBook-Air-10.local>
2025-11-27 16:19:31 -0500

    ah well nevertheless

---
+++ scripts/gen_vae_test.py
@@ -0,0 +1,11 @@
+for encoder_type in ["vae"]:
+    for grad in [2]:
+        for lr in [0.1]:
+            for zone in ["EmeraldHillZone","HillTopZone"]:
+                name=f"{encoder_type}_{grad}_{lr}_{zone}"
+                data=f"jlbaker361/discrete_{zone}.Act1100"
+                command=f" sbatch -J vae --constraint=L40S --err=slurm_chip/encoder/{name}.err --out=slurm_chip/encoder/{name}.out runpygpu_chip_l40.sh sonic_vae.py "
+                command+=f" --name  jlbaker361/sonic-encoder-{name} --save_dir sonic_encoder_{name}  --encoder_type  {encoder_type}  --project_name sonic-encoding --gradient_accumulation_steps {grad} "
+                command+=f" --lr {lr}  --epochs 2"
+                command+=f" --process_data --skip_num 2 --src_dataset {data} --load_hf "
+                print(command)
