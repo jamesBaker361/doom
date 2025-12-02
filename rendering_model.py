587f47b57af74c66a1fe47ab577452e9bab77ac5
James Baker <jamesbaker@Jamess-MacBook-Air-104.local>
2025-12-01 21:32:01 -0500

    init

--- rendering_model.py
+++ rendering_model.py
@@ -40,19 +40,9 @@ except ImportError:
 from diffusers.models.autoencoders.vae import DiagonalGaussianDistribution
 from huggingface_hub import create_repo,HfApi
 from data_loaders import VelocityPositionDatasetHF,RenderingModelDatasetHF
+from experiment_helpers.init_helpers import default_parser,repo_api_init
 
-parser=argparse.ArgumentParser()
-parser.add_argument("--mixed_precision",type=str,default="fp16")
-parser.add_argument("--project_name",type=str,default="person")
-parser.add_argument("--gradient_accumulation_steps",type=int,default=4)
-parser.add_argument("--repo_id",type=str,default="jlbaker361/model",help="name on hf")
-parser.add_argument("--lr",type=float,default=0.0001)
-parser.add_argument("--epochs",type=int,default=100)
-parser.add_argument("--limit",type=int,default=-1)
-parser.add_argument("--save_dir",type=str,default="weights")
-parser.add_argument("--batch_size",type=int,default=4)
-parser.add_argument("--load_hf",action="store_true")
-parser.add_argument("--val_interval",type=int,default=10)
+parser=default_parser()
 parser.add_argument("--action",type=str,default="embedding",help="encoder or embedding")
 parser.add_argument("--dataset",type=str,default="jlbaker361/discrete_HillTopZone.Act1100")
 parser.add_argument("--vae_checkpoint",type=str,default="jlbaker361/sonic-vae")
@@ -84,27 +74,7 @@ class ActionEncoder(torch.nn.Module):
             
 
 def main(args):
-    accelerator=Accelerator(log_with="wandb",mixed_precision=args.mixed_precision,gradient_accumulation_steps=args.gradient_accumulation_steps)
-    set_seed(123)
-    print("accelerator device",accelerator.device)
-    device=accelerator.device
-    state = PartialState()
-    print(f"Rank {state.process_index} initialized successfully")
-    if accelerator.is_main_process or state.num_processes==1:
-        accelerator.print(f"main process = {state.process_index}")
-    if accelerator.is_main_process or state.num_processes==1:
-        try:
-            accelerator.init_trackers(project_name=args.project_name,config=vars(args))
-
-            api=HfApi()
-            api.create_repo(args.repo_id,exist_ok=True)
-        except HfHubHTTPError:
-            print("hf hub error!")
-            time.sleep(random.randint(5,120))
-            accelerator.init_trackers(project_name=args.project_name,config=vars(args))
-
-            api=HfApi()
-            api.create_repo(args.repo_id,exist_ok=True)
+    api,accelerator=repo_api_init(args)
 
     
     pipe=DiffusionPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7")
@@ -117,19 +87,11 @@ def main(args):
     vae=pipe.vae
     image_processor=pipe.image_processor
 
-    torch_dtype={
-        "no":torch.float32,
-        "fp16":torch.float16,
-        "bf16":torch.bfloat16
-    }[args.mixed_precision]
-
     #dataset=??????
 
     dataset=RenderingModelDatasetHF(args.dataset,image_processor,["x","y"],True,
                                     None)
     n_actions=dataset.n_actions
-    test_size=int(len(dataset)//4)
-    train_size=int(len(dataset)-2*test_size)
     
     for batch in dataset:
         break
