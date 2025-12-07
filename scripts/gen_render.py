c023f9d49fe3290f40d612b11862c3a2e9cd5402
James Baker <jamesbaker@Jamess-MacBook-Air-132.local>
2025-12-07 12:20:19 -0500

    render more

--- scripts/gen_render.py
+++ scripts/gen_render.py
@@ -1,12 +1,16 @@
-for vae_checkpoint in ["none",]:
+for vae_checkpoint in ["default","none"]:
     for action in ["encoder","embedding"]:
         for grad in [16]:
-            for lr in [0.001,0.0001]:
+            for lr in [0.0001,0.001]:
                 for zone in ["EmeraldHillZone"]:
                     name=f"{action}_{lr}_{grad}_{vae_checkpoint}"
+                    checkpoint={
+                        "default":"jlbaker361/sonic-encoder-vae_16_1e-05_EmeraldHillZone"
+                        ,"none":""
+                    }[vae_checkpoint]
                     data=f"jlbaker361/discrete_{zone}.Act15000000" #jlbaker361/discrete_HillTopZone.Act15000000
                     command=f" sbatch -J render --constraint=L40S --err=slurm_chip/render/{name}.err --out=slurm_chip/render/{name}.out runpygpu_chip_l40.sh rendering_model.py "
                     command+=f" --repo_id  jlbaker361/rendering-{name} --save_dir rendering_{name}   --project_name sonic-rendering --gradient_accumulation_steps {grad} "
                     command+=f" --lr {lr}  --epochs 100"
-                    command+=f"   --dataset {data} --vae_checkpoint {vae_checkpoint} "
+                    command+=f"   --dataset {data} --vae_checkpoint {checkpoint} --use_lora"
                     print(command)
