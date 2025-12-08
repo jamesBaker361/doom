95fd1d0d97503bc1948c5d1fb24009d562d60907
James Baker <jlbaker361@gmail.com>
2025-12-08 13:35:57 -0500

    ?

--- play.py
+++ play.py
@@ -4,106 +4,166 @@ import numpy as np
 import os
 import time
 from PIL import Image
+import argparse
+import csv
 
-# ---- SETTINGS ----
-RECORD_DIR = "records_user"
-ACTIONS_FILE = "records_user/actions.npy"
-os.makedirs(RECORD_DIR, exist_ok=True)
 
-# Create environment with recording enabled
-env = retro.make(
-    game='SonicTheHedgehog2-Genesis',
-    #state='GreenHillZone.Act1',
-    record=RECORD_DIR,
-    use_restricted_actions=retro.Actions.ALL
-)
 
-# ---- BUTTON MAPPING ----
-# retro uses a fixed order of buttons depending on the game:
-# Example Genesis mapping: ['B','NULL','C','A','Start','Up','Down','Left','Right']
-BUTTONS = env.buttons
-print(BUTTONS)
-print(env.unwrapped.buttons)
+if __name__=='__main__':
 
-# Keyboard → Genesis button
-KEY_TO_BUTTON = {
-    pygame.K_d: "RIGHT",
-    pygame.K_a: "LEFT",
-    pygame.K_w: "UP",
-    pygame.K_s: "DOWN",
+    states={
+        'SonicTheHedgehog2-Genesis':['EmeraldHillZone.Act1','AquaticRuinZone.Act1','CasinoNightZone.Act1','HillTopZone.Act1'],
+        'SuperMarioWorld-Snes':["ChocolateIsland1",'DonutPlains1','Forest1','VanillaDome1'],
+        'CastlevaniaBloodlines-Genesis-v0':['Level1-1','Level2-1','Level3-1','Level4-1']
+    }
 
-    pygame.K_q: "B",      # Jump
-    pygame.K_x: "LEFT",
-    pygame.K_c: "C",
+    state_help=""
+    for k,v in states.items():
+        state_help+=f"{k} has states: "+",".join(v)+"\n"
 
-    pygame.K_RETURN: "Start",
-}
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--game",type=str,default='SonicTheHedgehog2-Genesis',help="one of \n SuperMarioWorld-Snes \n CastlevaniaBloodlines-Genesis-v0 \n SonicTheHedgehog2-Genesis ")
+    parser.add_argument("--state",type=str,default='EmeraldHillZone.Act1',help=state_help)
+    parser.add_argument("--save_dir",type=str,default="videos")
 
-# Reverse lookup: button name → index
-button_index = {b: i for i, b in enumerate(BUTTONS)}
+    args=parser.parse_args()
 
-# ---- Init pygame ----
-pygame.init()
-screen = pygame.display.set_mode((400, 200))
-pygame.display.set_caption("Sonic Controller - Focus this window to play!")
+    # ---- SETTINGS ----
+    save_parent=os.path.join(args.save_dir,args.game,args.state)
+    os.makedirs(save_parent, exist_ok=True)
+    n_games=len(os.listdir(save_parent))
+    save_dir=os.path.join(save_parent , str(n_games))
+    os.makedirs(save_dir,exist_ok=True)
 
-clock = pygame.time.Clock()
+    # Create environment with recording enabled
+    env = retro.make(
+        game=args.game,
+        state=args.state,
+        record=save_dir,
+        use_restricted_actions=retro.Actions.ALL
+    )
 
-obs = env.reset()
-done = False
+    # ---- BUTTON MAPPING ----
+    # retro uses a fixed order of buttons depending on the game:
+    # Example Genesis mapping: ['B','NULL','C','A','Start','Up','Down','Left','Right']
+    BUTTONS = env.buttons
+    print(BUTTONS)
+    print(env.unwrapped.buttons)
 
-# Recorded list of full button vectors
-action_log = []
+    # Keyboard → Genesis button
+    KEY_TO_BUTTON = {
+        pygame.K_d: "RIGHT",
+        pygame.K_a: "LEFT",
+        pygame.K_w: "UP",
+        pygame.K_s: "DOWN",
 
-print("Controls:")
-print(" Arrow keys = Move")
-print(" Z/X/C = Buttons (jump/spin)")
-print(" Enter = Start")
-print(" ESC = Quit")
-print("------------------")
-count=0
-try:
-    while not done:
-        count+=1
-        pressed = pygame.key.get_pressed()
-        action = np.zeros(len(BUTTONS), dtype=np.int8)
+        pygame.K_q: "B",      # Jump
+        #pygame.K_x: "LEFT",
+        #.K_c: "C",
 
-        for key, button_name in KEY_TO_BUTTON.items():
-            if pressed[key]:
-                idx = button_index.get(button_name, None)
-                if idx is not None:
-                    action[idx] = 1
+        pygame.K_RETURN: "Start",
+    }
 
-        # Per-step events (quit, ESC, etc.)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                done = True
+    # Reverse lookup: button name → index
+    button_index = {b: i for i, b in enumerate(BUTTONS)}
 
-        # Step emulator
-        
-        obs, rew, terminated, truncated, info=env.step(action)
-        if count==1:
+    # ---- Init pygame ----
+    pygame.init()
+    screen = pygame.display.set_mode((400, 200))
+    pygame.display.set_caption(" Controller - Focus this window to play!")
+
+    clock = pygame.time.Clock()
+
+    obs = env.reset()
+    done = False
+
+    # Recorded list of full button vectors
+    action_log = []
+
+    print("Controls:")
+    print(" Arrow keys = Move")
+    print(" Z/X/C = Buttons (jump/spin)")
+    print(" Enter = Start")
+    print(" ESC = Quit")
+    print("------------------")
+    PATH="videos"
+    os.makedirs(PATH,exist_ok=True)
+    count=0
+    action = np.zeros(len(BUTTONS), dtype=np.int8)
+    obs, rew, terminated, truncated, info=env.step(action)
+    env.reset()
+    output_dict={
+        key:[] for key in info
+    }
+    output_dict["action"]=[]
+    output_dict["save_path"]=[]
+    try:
+        while not done:
+            count+=1
+            if count>=1000:
+                break
+            pressed = pygame.key.get_pressed()
+            action = np.zeros(len(BUTTONS), dtype=np.int8)
+
+            for key, button_name in KEY_TO_BUTTON.items():
+                if pressed[key]:
+                    output_dict["action"].append(button_name)
+                    idx = button_index.get(button_name, None)
+                    if idx is not None:
+                        action[idx] = 1
+                    break
+            if np.sum(action)<1:
+                output_dict["action"].append("None")
+
+            # Per-step events (quit, ESC, etc.)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    done = True
+
+            # Step emulator
+            
+            obs, rew, terminated, truncated, info=env.step(action)
+
             image=obs
             image = Image.fromarray(image)
-            image.save("first.png")
-        env.render()
+            save_path_image=os.path.join(save_dir,f"{count}.jpg")
+            image.save(save_path_image)
+            env.render()
 
-        # Save user action
-        action_log.append(action.copy())
+            # Save user action
+            action_log.append(action.copy())
 
-        clock.tick(60)  # limit to 60 FPS for smooth control
+            clock.tick(60)  # limit to 60 FPS for smooth control
 
-except KeyboardInterrupt:
-    pass
+            for key in info:
+                output_dict[key].append(info[key])
+            output_dict['save_path'].append(save_path_image)
 
-finally:
-    env.close()
-    pygame.quit()
+    except KeyboardInterrupt:
+        pass
 
-    # Save the action sequence in numpy format
-    np.save(ACTIONS_FILE, np.array(action_log, dtype=np.int8))
-    print(f"Saved {len(action_log)} actions to {ACTIONS_FILE}")
+    finally:
+        env.close()
+        pygame.quit()
 
-    print("\nA .bk2 file has also been created in:", RECORD_DIR)
+        # Save the action sequence in numpy format
+        #np.save(ACTIONS_FILE, np.array(action_log, dtype=np.int8))
+        #print(f"Saved {len(action_log)} actions to {ACTIONS_FILE}")
+
+        #print("\nA .bk2 file has also been created in:", RECORD_DIR)
+
+        with open(os.path.join(save_dir,"data.csv"), "w+") as outfile:
+
+            # pass the csv file to csv.writer function.
+            writer = csv.writer(outfile)
+
+            # pass the dictionary keys to writerow
+            # function to frame the columns of the csv file
+            writer.writerow(output_dict.keys())
+        
+            # make use of writerows function to append
+            # the remaining values to the corresponding
+            # columns using zip function.
+            writer.writerows(zip(*output_dict.values()))
