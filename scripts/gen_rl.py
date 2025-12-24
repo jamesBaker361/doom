a67f5e5203413a9c9037c68e583b18c9ab829532
James Baker <jamesbaker@Jamess-MacBook-Air-331.local>
2025-12-24 00:37:29 -0500

    rl

--- scripts/gen_rl.py
+++ scripts/gen_rl.py
@@ -13,8 +13,8 @@ from shared import game_state_dict
 
 
 for game,values in game_state_dict.items():
-    for state in values[:1]:
+    for state in values:
         name=f"{game}_{state}"
         command=f"sbatch -J rl --err=slurm_chip/rl/{name}.err --out=slurm_chip/rl/{name}.out runpygpu_chip.sh retrovideo_memory.py --game {game} --scenario {state} --hard_coded_steps 0 "
-        command+=f" --timesteps 100 --repo_id jlbaker/{name}_agent  --dest_dataset  jlbaker/{name}_test_data --repo_id jlbaker/{name}_test_a "
+        command+=f" --max_episode_steps 5000  --dest_dataset  jlbaker361/{name}_rl --repo_id jlbaker361/{name}_rl_model  --n_episodes 100"
         print(command)
