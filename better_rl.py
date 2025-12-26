db08d33ad284dcf88ce382597fdda012e1f37079
James Baker <jamesbaker@Jamess-MacBook-Air-331.local>
2025-12-25 21:54:35 -0500

    nn

--- better_rl.py
+++ better_rl.py
@@ -24,6 +24,7 @@ from gymnasium.spaces import Box
 import numpy as np
 import time, datetime
 import matplotlib.pyplot as plt
+from agent_rl import Agent
 
 
 class MetricLogger:
@@ -231,5 +232,45 @@ if __name__=='__main__':
     print()
 
     save_dir = os.path.join("checkpoints",GAME)
+    os.makedirs(save_dir,exist_ok=True)
     
-    
+    mario = Agent(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir)
+
+    logger = MetricLogger(save_dir)
+
+    episodes = 40
+    for e in range(episodes):
+
+        state = env.reset()
+
+        # Play the game!
+        while True:
+
+            # Run agent on the state
+            action = mario.act(state)
+
+            # Agent performs action
+            next_state, reward, done, trunc, info = env.step(action)
+
+            # Remember
+            mario.cache(state, next_state, action, reward, done)
+
+            # Learn
+            q, loss = mario.learn()
+
+            # Logging
+            logger.log_step(reward, loss, q)
+
+            # Update state
+            state = next_state
+
+            # Check if end of game
+            if done or info["flag_get"]:
+                break
+
+        logger.log_episode()
+
+        if (e % 20 == 0) or (e == episodes - 1):
+            logger.record(episode=e, epsilon=mario.exploration_rate, step=mario.curr_step)
+        
+        
