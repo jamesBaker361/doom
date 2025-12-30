2e3a0e96ff8b905b3952b9456bfb83d13b54b589
James Baker <jamesbaker@Jamess-MacBook-Air-331.local>
2025-12-29 20:16:18 -0500

    gif script

---
+++ gif_from_episode.py
@@ -0,0 +1,22 @@
+from datasets import load_dataset
+from PIL import Image
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--src_dataset",type=str,default="jlbaker361/SuperMarioWorld-Snes_DonutPlains1_rl_data")
+parser.add_argument("--episode_list",type=int,nargs="*",default=[1])
+
+args=parser.parse_args()
+
+data=load_dataset(args.src_dataset,split="train")
+data_name=args.src_dataset.split("/")[-1]
+episode_set=set([e for e in data["episode"]])
+for e in args.episode_list:
+    if e in episode_set:
+        episode_data=data.filter(lambda row: row["episode"]==e)["episode"]
+        episode_data[0].save(f"{data_name}_episode_{e}.gif",save_all=True,append_images=episode_data[1:],optimize=False,duration=len(episode_data)/4)
+        
+if -1 in args.episode_list:
+    biggest=max(episode_set)
+    episode_data=data.filter(lambda row: row["episode"]==biggest)["episode"]
+    episode_data[0].save(f"{data_name}_episode_final.gif",save_all=True,append_images=episode_data[1:],optimize=False,duration=len(episode_data)/4)
