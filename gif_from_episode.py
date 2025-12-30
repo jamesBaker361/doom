03d4996d5c9600080130f481dcf87b077be07d0d
James Baker <jamesbaker@Jamess-MacBook-Air-331.local>
2025-12-30 17:57:50 -0500

    image

--- gif_from_episode.py
+++ gif_from_episode.py
@@ -13,10 +13,10 @@ data_name=args.src_dataset.split("/")[-1]
 episode_set=set([e for e in data["episode"]])
 for e in args.episode_list:
     if e in episode_set:
-        episode_data=data.filter(lambda row: row["episode"]==e)["episode"]
+        episode_data=data.filter(lambda row: row["episode"]==e)["image"]
         episode_data[0].save(f"{data_name}_episode_{e}.gif",save_all=True,append_images=episode_data[1:],optimize=False,duration=len(episode_data)/4)
         
 if -1 in args.episode_list:
     biggest=max(episode_set)
-    episode_data=data.filter(lambda row: row["episode"]==biggest)["episode"]
+    episode_data=data.filter(lambda row: row["episode"]==biggest)["image"]
     episode_data[0].save(f"{data_name}_episode_final.gif",save_all=True,append_images=episode_data[1:],optimize=False,duration=len(episode_data)/4)
